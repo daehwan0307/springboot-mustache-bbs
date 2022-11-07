f88a8bea52cd57e11bc264b75d82f70fda99ee93
daehwan0307 <eoghks960307@naver.com>
2022-11-07 14:05:34 +0900

    view 수정 1. greetings.mustache 수정

--- src/main/java/com/mustache/bbs/controller/MustacheController.java
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -15,7 +15,7 @@ public class MustacheController {
     @GetMapping(value = "/hi/{id}")
     public String mustacheCon2(@PathVariable String id, Model model) {
         model.addAttribute("username", "rok");// view에 값을 넘기겠
-        model.addAttribute("id", id);// view에 값을 넘기겠
+        model.addAttribute("id", id);// view에 값을 넘기겠다.
         return "greetings"; // greetings라는 이름의의 view를 리턴
     }
 }
