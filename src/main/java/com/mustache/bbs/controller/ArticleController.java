45a09c153f84a95c30a3b149abcddff0466b6660
daehwan0307 <eoghks960307@naver.com>
2022-11-07 14:09:19 +0900

    1. greetings.mustache 수정 2. ArticleController.java 추가

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -1,4 +1,16 @@
 package com.mustache.bbs.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j // 로깅을 위한 어노테이션 log를 사용할 수있다.
 public class ArticleController {
+    @GetMapping(value = "/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
 }
+
