d440865ac920d64d5a4d24613bb396cf5c19030e
daehwan0307 <eoghks960307@naver.com>
2022-11-08 15:55:34 +0900

    1.ArticleController 수정 2.View 추가

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -5,9 +5,13 @@ import com.mustache.bbs.domain.entity.Article;
 import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -21,6 +25,10 @@ public class ArticleController {
     public String newArticleForm() {
         return "articles/new";
     }
+    @GetMapping(value = "/list")
+    public String list() {
+        return "list";
+    }
 
     @PostMapping(value = "/posts")
     public String createArticle(ArticleDto form) {
@@ -29,5 +37,27 @@ public class ArticleController {
         articleRepository.save(article);
         return "";
     }
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if (!optArticle.isEmpty()) {
+            // Optional.get() ---> Article
+            model.addAttribute("article", optArticle.get());
+            return "show";
+        } else {
+            return "error";
+        }
+    }
+    @PostMapping("")
+    public String add(ArticleDto articleDto) {
+        log.info(articleDto.getTitle());
+        Article savedArticle = articleRepository.save(articleDto.toEntity());
+        log.info("generatedId:{}", savedArticle.getId());
+        // souf %d %s
+        return String.format("redirect:/articles/%d", savedArticle.getId());
+    }
+
+
 }
 
