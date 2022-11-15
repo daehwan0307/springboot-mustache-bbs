c7bae62bc29b10d86239df91e2b58c7f6aafeb8e
daehwan0307 <eoghks960307@naver.com>
2022-11-15 13:19:45 +0900

    1.TestCode 확인

--- src/main/java/com/mustache/bbs/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs/repository/HospitalRepository.java
@@ -3,5 +3,17 @@ package com.mustache.bbs.repository;
 import com.mustache.bbs.domain.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+    List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
+
+    List<Hospital> findByHospitalNameStartsWith(String keyword); // 시작
+
+    List<Hospital> findByHospitalNameEndsWith(String keyword); // 끝남
+
+    List<Hospital> findByPatientRoomCountGreaterThanAndPatientRoomCountLessThan(int var1, int var2);
+
+    List<Hospital> findByPatientRoomCountBetween(int a, int b);
 }
--- src/test/java/com/mustache/bbs/SpringbootMustacheBbsApplicationTests.java
+++ src/test/java/com/mustache/bbs/SpringbootMustacheBbsApplicationTests.java
@@ -1,10 +1,69 @@
 package com.mustache.bbs;
 
+import com.mustache.bbs.domain.entity.Hospital;
+import com.mustache.bbs.repository.HospitalRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class SpringbootMustacheBbsApplicationTests {
+class HospitalRepositoryTest {
 
+    @Autowired
+    HospitalRepository hospitalRepository;
 
+    @Test
+    @DisplayName("BusinessTypeName이 보건소 보건지소 보건진료소인 데이터가 잘 나오는지")
+    void findByBusinessTypeNameIn() {
+        List<String> inClues = new ArrayList<>();
+        inClues.add("보건소");
+        inClues.add("보건지소");
+        inClues.add("보건진료소");
+        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameIn(inClues);
+
+        for (var hospital : hospitals) {
+            System.out.println(hospital.getHospitalName());
+        }
+    }
+
+    @Test
+    void containing() {
+        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining("송파구");
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void startWith() {
+        List<Hospital> hospitals = hospitalRepository.findByHospitalNameStartsWith("경희");
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void endsWith() {
+        List<Hospital> hospitals = hospitalRepository.findByHospitalNameEndsWith("병원");
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    void printHospitalNameAndAddress(List<Hospital> hospitals) {
+        for (var hospital : hospitals) {
+            System.out.printf("%s | %s %f\n", hospital.getHospitalName(), hospital.getRoadNameAddress(), hospital.getTotalAreaSize());
+        }
+
+        System.out.println(hospitals.size());
+    }
+
+    @Test
+    void findById() {
+        Optional<Hospital> hospital = hospitalRepository.findById(1);
+        Hospital hp = hospital.get();
+        System.out.println(hp.getId());
+        assertEquals(1,hp.getId());
+    }
 }
